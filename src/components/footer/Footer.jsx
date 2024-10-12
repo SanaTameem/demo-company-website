5d8a7352338347646440d3694a4208231943c580
unknown <sanatameem38@gmail.com>
2024-10-12 19:30:12 +0430

    Add footer directory

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { links } from "../links/links";
+
+function Footer() {
+  return (
+    <footer className="py-3 my-4">
+      <div className="container fixed-bottom">
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+          {links.map((link) => (
+            <li className="nav-item" key={link.title}>
+              <a href={link.path} className="nav-link px-2 text-body-secondary">
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <p className="text-center text-body-secondary">© 2024 EliteBarry, Inc</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
