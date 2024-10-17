48eda59b0d4664e38fdfa7a10becafc0caea30b4
unknown <sanatameem38@gmail.com>
2024-10-17 17:35:23 +0430

    Update links.js file

--- src/components/links/links.js
+++ src/components/links/links.js
@@ -1,24 +1,32 @@
 export let links = [
+  { title: "Home", path: "/", sub_categories: [] },
   {
-    title: "Home",
-    path: "/",
+    title: "About",
+    path: "#about",
     sub_categories: [
-      "Company Background",
-      "Why us?",
-      "Our Team",
-      "Our Partners",
-      "Our Goals and Missions",
+      { sub_name: "Company Background", sub_path: "#company_background" },
+      { sub_name: "Why us?", sub_path: "#why_us" },
+      { sub_name: "Our Team", sub_path: "#our_team" },
+      { sub_name: "Our Partners", sub_path: "#our_partners" },
+      { sub_name: "Our Goals and Missions", sub_path: "#goals_missions" },
     ],
   },
   {
     title: "Services",
     path: "/services",
-    sub_categories: ["Software Development", "Networking", "Consultancy"],
+    sub_categories: [
+      { sub_name: "Software Development", sub_path: "#software" },
+      { sub_name: "Networking", sub_path: "#networking" },
+      { sub_name: "Consultancy", sub_path: "#consultancy" },
+    ],
   },
   {
     title: "Career",
     path: "/career",
-    sub_categories: ["Training Programs", "Job Openings"],
+    sub_categories: [
+      { sub_name: "Training Programs", sub_path: "#training_programs" },
+      { sub_name: "Job Openings", sub_path: "#job_openings" },
+    ],
   },
   { title: "Blog", path: "/blog", sub_categories: [] },
   { title: "Contact", path: "/contact", sub_categories: [] },
