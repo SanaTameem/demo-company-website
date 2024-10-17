df6a281073ba7f987ba387a48677cf210a04b2b1
unknown <sanatameem38@gmail.com>
2024-10-17 17:35:24 +0430

    Update Navbar.jsx file

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,35 @@
+"use client"
 import Image from "next/image";
 import "./navbar.css";
 import Links from "../links/Links.jsx";
+import { useEffect } from "react";
 
 function Navbar() {
+  useEffect(() => {
+    const navbar = document.querySelector(".navbar");
+
+    const userScroll = () => {
+      if (window.scrollY > 70) {
+        navbar.classList.add("bg-secondary");
+        navbar.classList.remove("bg-white");
+      } else {
+        navbar.classList.remove("bg-secondary");
+        navbar.classList.add("bg-white");
+      }
+    };
+
+    window.addEventListener("scroll", userScroll);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener("scroll", userScroll);
+    };
+  }, []); // Empty dependency array to run on mount and unmount
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+    <nav className="navbar navbar-expand-lg bg-white fixed-top">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          <Image src="/logo.jpg" alt="logo" width="70" height="60" />
+          <Image src="/logo.jpg" alt="logo" width="50" height="60" />
         </a>
         <button
           className="navbar-toggler"
@@ -22,17 +44,6 @@ function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <Links />
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-primary" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
